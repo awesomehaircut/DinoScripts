5dad3297f7655a75497f4ab68e8e2e073e15819b
AwesomeHaircuT <jesusbaslbastro@gmail.com>
2018-08-17 09:26:34 -0700

    initial commit

---
+++ GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public float speed;
+	public int size;
+	public int amount;
+	public int frequency;
+	public GameObject obstacle;
+	public Vector3 spawnValue;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.FindGameObjectsWithTag("Cacti").Length < amount)
+		{
+			spawn_cacti();
+		}
+		
+	}
+	void spawn_cacti(){
+		Vector3 spawnPosition = new Vector3 (spawnValue.x, spawnValue.y, spawnValue.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (obstacle, spawnPosition, spawnRotation);
+	}
+}
+
---
+++ Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public float speed = 0.001f;
+	public Vector3 direction = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		direction.x = 1 * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller  = GetComponent<CharacterController>();
+		transform.Translate(direction.x,0,0);
+		
+	}
+}
